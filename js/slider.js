89e771073ea3193a219c41dbc16222842c55d1fe
SvetlanaAleks <annael@ukr.net>
2022-05-02 13:42:26 +0300

    add loader / add pagination

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,21 @@
-$("#slider").slick({
-  dots: true,
-  slidesToShow: 1,
+function sliderInit() {
+  var status = $(".slides-numbers");
+  var slider = $(".slider");
+  slider.on(
+    "init reInit afterChange",
+    function (event, slick, currentSlide, nextSlide) {
+      //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+      var i = (currentSlide ? currentSlide : 0) + 1;
+      status.html(
+        `<span class="active">${i}</span> / <span class="total">${slick.slideCount}</span>`
+      );
+    }
+  );
+  slider.slick({
+    dots: false,
+    slidesToShow: 1,
+  });
+}
+$(document).ready(function () {
+  sliderInit();
 });
